c84f04d68a9f2ff1d5653c9ae9274c5248c9960c
cheesemaster <eric6iese@googlemail.com>
2015-12-07 17:15:06 +0100

    Das Projekt wird intern in jautoit umbenannt da es zunächst ausschließlich diesen Job übernehmen soll.

--- scriptmaster/jautoit-test/src/test/java/de/evermind/AutoItXExample.java
+++ scriptmaster/jautoit-test/src/test/java/de/evermind/AutoItXExample.java
@@ -15,24 +15,26 @@ import de.evermind.scriptmaster.jacob.JacobLoader;
 public class AutoItXExample {
 
 	/**
-	 * There are some glaring problems with using the auto-api as is, as autoitx shows.<br/>
+	 * There are some glaring problems with using the auto-api as is, as autoitx
+	 * shows.<br/>
 	 * I guess i will have to make some minor improvements to them.
 	 */
 	@Test
-	public void testAutoit() {
+	public void testAutoit() throws Exception {
 		JacobLoader.initialize();
 		AutoItX x = new AutoItX();
 		String notepad = "Unbenannt - Editor";
 		String testString = "this is a test.";
+
+		Runtime.getRuntime().exec("cmd /c notepad");
 		// x.run("notepad.exe");
 		x.winActivate(notepad);
-		 x.winWaitActive(notepad);
+		x.winWaitActive(notepad);
 		x.send(testString);
 		assertTrue(x.winExists(notepad, testString));
 		x.winClose(notepad, testString);
-		x.winWaitActive("Notepad");
+		x.winWaitActive("Editor");
 		x.send("{ALT}n");
 		assertFalse(x.winExists(notepad, testString));
 	}
-
 }
