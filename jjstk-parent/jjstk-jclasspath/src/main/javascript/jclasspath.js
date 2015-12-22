c3bf2fa878f967a5cfea76734c7544327b1a20f2
cheesemaster <eric6iese@googlemail.com>
2015-12-22 09:44:09 +0100

    jclasspath now implements the classpathloading in the javascript. the maven parts are now only evaluated (once) when requireArtifact is called

--- jjstk-parent/jjstk-jclasspath/src/main/javascript/jclasspath.js
+++ jjstk-parent/jjstk-jclasspath/src/main/javascript/jclasspath.js
@@ -1,37 +1,68 @@
 // Imports
 var File = java.io.File;
 var URLClassLoader = java.net.URLClassLoader;
+var URL = java.net.URL;
+var Thread = java.lang.Thread;
+
+var mAddUrl = null;
 
 /**
- * Load all jars of the distribution in a separate classloader to avoid version
- * collisions.
+ * Loads associated resolved Dependencies into the classpath.
  */
-var className = "de.evermind.scriptmaster.aether.DependencyManager";
-var libDir = new File(__dirname, "lib");
-if (!libDir.isDirectory()){
-    throw new Error("Cannot find directory " + libDir + "!");
-}
+var requireAll = function (files) {
+    if (mAddUrl === null) {
+        mAddUrl = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+        mAddUrl.setAccessible(true);
+    }
+    var classLoader = Thread.currentThread().getContextClassLoader();
 
-var libJars = Java.from(libDir.listFiles()).filter(function(f) f.getName().endsWith(".jar") || f.isDirectory());
-var libUrls = libJars.map(function(f) f.toURI().toURL());
-
-// Create the separate ClassLoader and finally load the class
-var classLoader = new java.net.URLClassLoader(libUrls);
-var clazz = classLoader.loadClass(className);
-	
-var dependencyManager = clazz.newInstance();
+    var urls = {};
+    Java.from(classLoader.getURLs()).forEach(function (it) {
+        urls[it.toString()] = true;
+    });
+    files.forEach(function (file) {
+        var url = file.toURI().toURL();
+        if (urls[url.toString()]) {
+            return;
+        }
+        mAddUrl.invoke(classLoader, url);
+    });
+};
 
 /**
  * Resolves the given argument as a jarfile to load in the local workspace.
  */
-exports.require = function(file){
-    dependencyManager.load([new File(file)]);
-}
+exports.require = function (file) {
+    requireAll([new File(file)]);
+};
+
+var dependencyManager = null;
 
 /**
  * Resolves the given argument as a dependency in the local workspace.
  */
-exports.requireArtifact = function(dependency) {
-	var files = dependencyManager.resolve([dependency]);
-	dependencyManager.load(files);
-}
+exports.requireArtifact = function (dependency) {
+    if (dependencyManager === null) {
+        // Load all jars of the distribution in a separate classloader to avoid version collisions.
+        var className = "de.evermind.scriptmaster.aether.DependencyManager";
+        var libDir = new File(__dirname, "lib");
+        if (!libDir.isDirectory()) {
+            throw new Error("Cannot find directory " + libDir + "!");
+        }
+
+        var libJars = Java.from(libDir.listFiles()).filter(function (f) {
+            return f.getName().endsWith(".jar") || f.isDirectory();
+        });
+        var libUrls = libJars.map(function (f) {
+            return f.toURI().toURL();
+        });
+
+        // Create the separate ClassLoader and finally load the class
+        var classLoader = new URLClassLoader(libUrls);
+        var clazz = classLoader.loadClass(className);
+
+        dependencyManager = clazz.newInstance();
+    }
+    var files = dependencyManager.resolve([dependency]);
+    requireAll(files);
+};
