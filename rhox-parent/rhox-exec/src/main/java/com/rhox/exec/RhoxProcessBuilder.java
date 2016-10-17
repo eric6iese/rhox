f260074cf21b85bdadb9f9707432495d0975a400
cheesemaster <eric6iese@googlemail.com>
2016-10-17 21:17:23 +0200

    Added rhox-exec a new library which will help to execute processes from javascripts. Its extremely beta for now (and does not even compile completely)

---
+++ rhox-parent/rhox-exec/src/main/java/com/rhox/exec/RhoxProcessBuilder.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rhox.exec;
+
+/**
+ * A ProcessExecutor Factory, simplified in many ways for shell-scripting. Its
+ * main properties are dir (workdir), in, out, err.
+ */
+public class RhoxProcessBuilder {
+
+}
